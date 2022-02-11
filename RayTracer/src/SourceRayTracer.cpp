bb0de8db09614dfd0bc858cffb8e15f122f328f8
Morgan Leskerpit <morgan.leskerpit@eleves.ec-nantes.fr>
2022-02-11 15:33:07 +0100

    Hello World en synthese d'image

--- RayTracer/src/SourceRayTracer.cpp
+++ RayTracer/src/SourceRayTracer.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <test.h>
+#include <fstream>
 
 int main() {
-    std::cout <<"Hello World" << std::endl;
-    helloHeader();
-    std::cout << "Quelle connerie Cmake" << std::endl;
+    std::ofstream renderImage("render.ppm");
 
+    int nx = 200;
+    int ny = 100;
+
+    renderImage << "P3\n" << nx << " " << ny << "\n255\n";
+
+    for (int j  = ny-1; j>=0; j--){
+        for (int i = 0; i <nx; i++) {
+            float r = float(i) / float(nx);
+            float g = float(j) / float (ny);
+            float b = 0.2f;
+
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
+            renderImage << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+
+    renderImage.close();
     system("pause");
     return 0;
 }
