9fa28d971bdeee26e0c8f89bfb81f25e6cd0ec1f
Morgan Leskerpit <morgan.leskerpit@eleves.ec-nantes.fr>
2022-02-22 18:35:53 +0100

    Jusqu a la partie sur l Antialiasing

--- RayTracer/src/SourceRayTracer.cpp
+++ RayTracer/src/SourceRayTracer.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <fstream>
 
-#include <vec3.h>
+#include <rtweekend.h>
 #include <color.h>
-#include <ray.h>
 #include <sphere.h>
+#include <hittable_list.h>
 
-color ray_color(const ray& r) { //Returns the color of the background (a gradient)
+color ray_color(const ray& r, const hittable& world) { //Returns the color of the background (a gradient)
 
-    //Ajout de l'intersection à la boule:
-    // auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    // if (t>0.0) {
-    //     vec3 N = unit_vector(r.at(t)- vec3(0, 0, -1)); //Vecteur normal a la sphere au point d'intersection
-    //     return 0.5*color(N.x() + 1, N.y() + 1, N.z() +1); //On se ramène a [0,1]
-    // }
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
+    }
 
-    //Gradient du ciel
+    //Gradient du ciel si on ne touche rien
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
@@ -29,8 +27,12 @@ int main() {
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width/aspect_ratio);
     
-    //Camera
+    //World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
+    //Camera
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
     auto focal_length = 1.0;
@@ -52,7 +54,7 @@ int main() {
             auto v = double(j) / (image_height - 1);
 
             ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(render_image, pixel_color);
         }
     }
