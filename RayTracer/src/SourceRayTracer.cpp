33305e8b305e89feb92635048852ecce47dac4cf
Morgan Leskerpit <morgan.leskerpit@eleves.ec-nantes.fr>
2022-02-25 08:28:35 +0100

    Final Render

--- RayTracer/src/SourceRayTracer.cpp
+++ RayTracer/src/SourceRayTracer.cpp
@@ -32,37 +32,25 @@ int main() {
 
 
     //Image
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
-    const int image_height = static_cast<int>(image_width/aspect_ratio);
-    const int samples_per_pixel = 100;
+    const auto aspect_ratio = 3.0 / 2.0;
+    const int image_width = 1200;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 500;
     const int max_depth = 50;
     
     //World
-    hittable_list world;
-
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    auto world = random_scene();
 
     //Camera
-    point3 lookfrom(3,3,2);
-    point3 lookat(0,0,-1);
+    point3 lookfrom(13,2,3);
+    point3 lookat(0,0,0);
     vec3 vup(0,1,0);
-    auto dist_to_focus = (lookfrom-lookat).length();
-    auto aperture = 2.0;
-
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     //Render
-    std::ofstream render_image("myDepthOfFieldRender.ppm");
+    std::ofstream render_image("myFinalRender.ppm");
 
     render_image << "P3\n" << image_width << " " << image_height << "\n255\n";
 
